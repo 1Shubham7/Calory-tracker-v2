70bc4f66b741ac72137e8f01fd7c4ce8943a3274
Shubham Singh <shubhammahar1306@gmail.com>
2023-09-17 17:51:25 +0530

    add: added a router and all the methods

---
+++ backend/handlers/handlers.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	""
+)
+
+func AddFoodEntry() {
+	
+}
+
+func GetFoodEntry(){
+	
+}
--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/1shubham7/caltech/handlers"
 )
 
 func main(){
 	
+	router := gin.New()
+	router.Use(gin.Logger)
+	router.Use(cors.Default)
+
+	router.GET("/food/:id", handlers.GetFoodEntry)
+	router.GET("/allfood/", handlers.GetAllFoodEntries)
+	router.GET("/food/:ingredient", handlers.GetFoodEntryByIngredient)
+
+	router.POST("/food/create", handlers.AddFoodEntry)
+
+	router.PUT("/food/update/:id", handlers.AddFoodEntry)
+	router.PUT("/food/update/:ingredient, handlers.AddFoodEntryByEngredient")
+
+	router.DELETE("/food/delete/:id", handlers.DeleteFoodEntry)
+
+	router.Run(":6000")
 }
