9d111dd3afd991ad2af8ee6d1bc0519c41207350
Shubham Singh <shubhammahar1306@gmail.com>
2023-09-26 02:45:29 +0530

    w: setup all components, design is ready. working on foodEntry

---
+++ frontend/src/components/foodEntry/FoodEntry.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function FoodEntry() {
+    const Entry = ({foodEntry, setChangeIngredient, deleteFoodEntry, setChangeEntry}) => {
+        // the struct we created in backend , all the data from there will come to foodEntry
+        return (
+            <div>
+              
+            </div>
+          )
+    }
+    
+
+}
