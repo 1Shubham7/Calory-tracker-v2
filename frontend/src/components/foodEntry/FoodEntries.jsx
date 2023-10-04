e599c15cdb6207a3baa21aee20ce0ca8cdaedf66
Shubham Singh <shubhammahar1306@gmail.com>
2023-10-04 20:02:10 +0530

    add: added the entire modal for frontend

--- frontend/src/components/foodEntry/FoodEntries.jsx
+++ frontend/src/components/foodEntry/FoodEntries.jsx
@@ -27,14 +27,72 @@ const Entries =() => {
 
   return (
     <>
-    <div className='container'>
-      <div onClick={() => setAddNewEntry(true)}>Today's calories</div>
-    </div>
-    <div className="container">
-      {entries != null && entries.map((entry, i)=>(
-        <Entry entryData={entry} setChangeIngredient={setChangeIngredient} setChangeEntry={setChangeEntry} deleteSingleEntry={deleteSingleEntry}/>
-      ))}
-    </div>
+    <div>
+            <Container>
+        <Button onClick={() => setAddNewEntry(true)}>Track today's calories</Button>
+            </Container>
+            <Container>
+        {entries != null && entries.map((entry, i) =>(
+            <Entry entryData={entry} deleteSingleEntry={deleteSingleEntry} setChangeIngredient={setChangeIngredient} setChangeEntry={setChangeEntry} />
+        ))}
+            </Container>
+
+            <Modal show={addNewEntry} onHide={() => setAddNewEntry(false)} centred>
+            <Modal.Header closeButton>
+            <Modal.Title>Add Calorie Entry</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <Form.Group>
+                    <Form.Label>dish</Form.Label>
+                    <Form.Control onChange={(event) => {newEntry.dish = event.target.value}}></Form.Control>
+                    <Form.Label>ingredients</Form.Label>
+                    <Form.Control onChange={(event) => {newEntry.ingredients = event.target.value}}></Form.Control>
+                    <Form.Label>calories</Form.Label>
+                    <Form.Control onChange={(event) => {newEntry.calories = event.target.value}}></Form.Control>
+                    <Form.Label>fat</Form.Label>
+                    <Form.Control type="number" onChange={(event) => {newEntry.fat = event.target.value}}></Form.Control>
+                </Form.Group>
+                <Button onClick={() => addSingleEntry()}>Add</Button>
+                <Button onClick={()=> setAddNewEntry(false)}>Cancel</Button>
+            </Modal.Body>
+            </Modal>
+
+            <Modal show={changeIngredient.change} onHide={() => setChangeIngredient({"change": false, "id":0})} centred>
+            <Modal.Header closeButton>
+                <Modal.Title>Change Ingredients</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <Form.Group>
+                    <Form.Label>new ingredients</Form.Label>
+                    <Form.Control onChange={(event) => {setNewIngredientName(event.target.value)}}></Form.Control>
+                </Form.Group>
+                <Button onClick={() => changeIngredientForEntry()}>Change</Button>
+                <Button onClick={() => setChangeIngredient({"change": false, "id":0})}>Cancel</Button>
+            </Modal.Body>
+            </Modal>
+
+            <Modal show={changeEntry.change} onHide={() => setChangeEntry({"change": false, "id":0})} centred>
+            <Modal.Header closeButton>
+                <Modal.Title>Change Entry</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form.Group>
+                    <Form.Label>dish</Form.Label>
+                    <Form.Control onChange={(event) => {newEntry.dish = event.target.value}}></Form.Control>
+                    <Form.Label>ingredients</Form.Label>
+                    <Form.Control onChange={(event) => {newEntry.ingredients = event.target.value}}></Form.Control>
+                    <Form.Label>calorie</Form.Label>
+                    <Form.Control onChange={(event) => {newEntry.calories = event.target.value}}></Form.Control>
+                    <Form.Label>fat</Form.Label>
+                    <Form.Control type="number" onChange={(event) => {newEntry.fat = event.target.value}}></Form.Control>
+                </Form.Group>
+                <Button onClick={() => changeSingleEntry()}>Change</Button>
+                <Button onClick={() => setChangeEntry({"change": false, "id":0})}>Cancel</Button>
+            </Modal.Body>
+        </Modal>
+        </div> 
     </>
   );
 
